fa655333703ec104dabb9d8852ccb26012e47dc0
Rahul Sarkar <8199829rs@gmail.com>
2023-04-17 19:39:29 +0530

    v0.5

--- src/components/homeCharts/Geo.js
+++ src/components/homeCharts/Geo.js
@@ -6,7 +6,7 @@ const Geo = () => {
     const TEXT_COLOR = "text-light-TXC-600 dark:text-dark-TXC-100"
     return (
         <div className={`col-span-4 3xl:col-span-6 lg:col-span-12 h-80 ${BACK_GRAOUND_COLOR} lg:overflow-x-scroll relative`}>
-            <h2 className={`block ${TEXT_COLOR} font-semibold pt-6 px-8 md:text-sm`}>Revenue Generated</h2>
+            <h2 className={`block ${TEXT_COLOR} font-semibold pt-6 px-8 md:text-sm`}>Geography Based Traffic</h2>
             <div className='h-[90%] w-full absolute bottom-0 p-7 lg:w-[50rem]'>
                 <MyResponsiveGeo data={geoData} legendsDisplay={false} projectionValue={150} />
             </div>
